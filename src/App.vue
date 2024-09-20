12a370e8a22cec791ee94902b2c8b446f5c790b5
Gustavo Nogueira <guuhnog@gmail.com>
2024-09-20 16:30:45 -0300

    Criando o html

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,37 @@
 <template>
-  <div></div>
+  <div
+    class="h-screen w-screen dark:bg-fundoEscuro bg-laranjaClaro flex flex-col items-center font-orbitron justify-center"
+  >
+    <!-- button to change theme -->
+    <div
+      class="flex items-center w-auto space-x-16 text-azulEsverdeado dark:text-azulEscuro font-bold"
+    >
+      <p>Tema :</p>
+      <div class="flex items-center space-x-4" @click="changeTheme()">
+        <span class="material-symbols-outlined text-3xl"> dark_mode </span>
+        <span
+          class="material-symbols-outlined text-5xl select-none cursor-pointer hover:text-azulEsverdeadoEscuro dark:hover:text-verdeClaro"
+          v-if="dark"
+        >
+          toggle_off
+        </span>
+        <span
+          class="material-symbols-outlined text-5xl cursor-pointer hover:text-azulEsverdeadoEscuro select-none dark:hover:text-verdeClaro"
+          v-else
+        >
+          toggle_on
+        </span>
+        <span class="material-symbols-outlined text-3xl"> light_mode </span>
+      </div>
+    </div>
+    <!-- __________End_________ -->
+    <!-- calculator screen -->
+    <div
+      class="container w-full h-24 max-h-24 rounded-md bg-beje dark:bg-azulMarinho text-xl font-bold text-fundoEscuro flex items-end justify-end p-1 break-words"
+    >
+      <p class="max-w-full" v-if="showOnScreen">{{ showOnScreen }}</p>
+      <p class="max-w-full" v-else>{{ result }}</p>
+    </div>
+    <!-- __________End_________ -->
+  </div>
 </template>
